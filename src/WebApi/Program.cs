3562778470887e33ea54a536ee852b12a171c3fd
mezenc3v <mezenc3v@gmail.com>
2019-06-24 00:49:19 +0300

    Initailize database on startup

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.EFCore;
+using Data.EFCore.Tests;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApi
 {
 	public class Program
 	{
 		public static void Main(string[] args)
-		{
-			CreateWebHostBuilder(args).Build().Run();
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+                DbInitializer.Initialize(context);
+            }
+            host.Run();
 		}
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
